929ab54fdb05a00475641450164670df0a4639b1
Alim Suma <sumaalim@gmail.com>
2024-04-19 22:14:00 +0800

    updated database

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,25 @@
 import { Sequelize } from "sequelize";
-
+import dotenv from "dotenv";
+dotenv.config();
 // const db = new Sequelize("secret", "root", "", {
 //   host: "localhost",
 //   dialect: "mysql",
 // });
+
+// const db = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USERNAME,
+//   process.env.PASSWORD,
+//   {
+//     host: process.env.HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: true,
+//       },
+//     },
+//   }
+// );
 
 const db = new Sequelize(
   process.env.DATABASE,
@@ -11,6 +27,7 @@ const db = new Sequelize(
   process.env.PASSWORD,
   {
     host: process.env.HOST,
+    port: process.env.PORT,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
